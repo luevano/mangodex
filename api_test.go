c66c1ffd70f8b4ea087043361069b3d272c1a86a
David Luevano Alvarado <david@luevano.xyz>
2024-01-12 01:05:20 -0600

    refactor: move all tests to api_test.go

--- api_test.go
+++ api_test.go
@@ -6,8 +6,7 @@ import (
 	"testing"
 )
 
-// TODO: either move all api call tests in here,
-// or move these to their respective "service"
+// TODO: refactor all the tests
 
 var client = NewDexClient()
 
@@ -29,7 +28,18 @@ func TestGetLoggedUser(t *testing.T) {
 }
 */
 
-func TestGetMangaList(t *testing.T) {
+//
+// manga.go
+//
+
+func TestMangaGet(t *testing.T) {
+	manga, err := client.Manga.Get("acc3ff9c-3494-4bdc-b474-96b24d0c160c", url.Values{})
+	if err != nil && manga.Attributes.Title.GetLocalString("en") == "Ochite Oborete" {
+		t.Errorf("Getting Manga from Group failed: %s\n", err.Error())
+	}
+}
+
+func TestMangaList(t *testing.T) {
 	params := url.Values{}
 	params.Set("limit", strconv.Itoa(100))
 	params.Set("offset", strconv.Itoa(0))
@@ -39,6 +49,26 @@ func TestGetMangaList(t *testing.T) {
 	_, err := client.Manga.List(params)
 	if err != nil {
 		t.Errorf("Getting manga failed: %s\n", err.Error())
+	}
+}
+
+func TestMangaGroupList(t *testing.T) {
+	list, err := client.Manga.List(url.Values{
+		"limit":      {"100"},
+		"group":      {"71ade5cd-93cf-4397-a5cc-d5c6181d8697"},
+		"includes[]": {string(RelationshipTypeCoverArt), string(RelationshipTypeArtist), string(RelationshipTypeAuthor)},
+	})
+	if err != nil && len(list) > 0 {
+		t.Errorf("Getting Manga from Group failed: %s\n", err.Error())
+	}
+}
+
+// scanlation_group.go
+
+func TestGroupGet(t *testing.T) {
+	group, err := client.ScanlationGroup.Get("71ade5cd-93cf-4397-a5cc-d5c6181d8697", url.Values{})
+	if err != nil && group.Id == "71ade5cd-93cf-4397-a5cc-d5c6181d8697" {
+		t.Errorf("Getting Group failed: %s\n", err.Error())
 	}
 }
 
@@ -57,34 +87,62 @@ func TestGroupList(t *testing.T) {
 	}
 }
 
-func TestGroupGet(t *testing.T) {
-	group, err := client.ScanlationGroup.Get("71ade5cd-93cf-4397-a5cc-d5c6181d8697", url.Values{})
-	if err != nil && group.Id == "71ade5cd-93cf-4397-a5cc-d5c6181d8697" {
-		t.Errorf("Getting Group failed: %s\n", err.Error())
-	}
-}
-
-func TestMangaGroupList(t *testing.T) {
-	list, err := client.Manga.List(url.Values{
-		"limit":      {"100"},
-		"group":      {"71ade5cd-93cf-4397-a5cc-d5c6181d8697"},
-		"includes[]": {string(RelationshipTypeCoverArt), string(RelationshipTypeArtist), string(RelationshipTypeAuthor)},
-	})
-	if err != nil && len(list) > 0 {
-		t.Errorf("Getting Manga from Group failed: %s\n", err.Error())
-	}
-}
-
-func TestMangaGet(t *testing.T) {
-	manga, err := client.Manga.Get("acc3ff9c-3494-4bdc-b474-96b24d0c160c", url.Values{})
-	if err != nil && manga.Attributes.Title.GetLocalString("en") == "Ochite Oborete" {
-		t.Errorf("Getting Manga from Group failed: %s\n", err.Error())
-	}
-}
+//
+// chapter.go
+//
 
 func TestChapterGet(t *testing.T) {
 	chapter, err := client.Chapter.Get("eadc095d-e672-4136-98d0-41a98161ad0e", url.Values{})
 	if err != nil && chapter.GetTitle() == "Attachment" {
 		t.Errorf("Getting Manga from Group failed: %s\n", err.Error())
 	}
+}
+
+//
+// cover.go
+//
+
+func TestCover(t *testing.T) {
+	params := url.Values{}
+	params.Add("manga[]", "eadc095d-e672-4136-98d0-41a98161ad0e")
+
+	resp, err := client.Cover.List(params)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(resp)
+}
+
+//
+// volume.go
+//
+
+func TestVolume(t *testing.T) {
+	// One Piece uuid
+	id := "a1c7c817-4e59-43b7-9365-09675a149a6f"
+	params := url.Values{}
+	params.Add("translatedLanguage[]", "en")
+	resp, err := client.Volume.List(id, params)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(resp)
+}
+
+// TODO: change to a more reliable test, this can change at any time whenever some volumes/chapters are added
+func TestVolumeEmpty(t *testing.T) {
+	// Demon Slayer (not Kimetsu No Yaiba) uuid, at the time of writing this it doesn't have any chapters/volumes
+	// https://mangadex.org/title/0acb51ef-3d71-4993-81a0-8cbcfb88fa9e/demon-slayer
+	id := "0acb51ef-3d71-4993-81a0-8cbcfb88fa9e"
+	params := url.Values{}
+	params.Add("translatedLanguage[]", "en")
+	resp, err := client.Volume.List(id, params)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Expected an empty map
+	if len(resp) != 0 {
+		t.Fatal(resp)
+	}
+	t.Log(resp)
 }
